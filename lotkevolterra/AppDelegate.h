54b8dbd3f1290142631c9a7af9686d3996665002
Jurgen Schwietering <scjurgen@yahoo.com>
2013-11-20 20:55:35 +0100

    Initial Commit

---
+++ lotkevolterra/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  lotkevolterra
+//
+//  Created by jay on 20/11/13.
+//  Copyright (c) 2013 Jurgen Schwietering. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ lotkevolterra/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  lotkevolterra
+//
+//  Created by jay on 20/11/13.
+//  Copyright (c) 2013 Jurgen Schwietering. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
